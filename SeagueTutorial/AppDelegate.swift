79d461548a42a8ce9ace98780fbe6ba1c9e5726e
michaelmoldawski <michaelmoldawski@hotmail.com>
2017-09-24 16:03:24 +0200

    test4

--- SeagueTutorial/AppDelegate.swift
+++ SeagueTutorial/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 let test = 5
     let test2 = 6
-
+let test3 = "string"
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
