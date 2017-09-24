e180eb43c66cc269d8df8d16cd226aa95ebd7430
michaelmoldawski <michaelmoldawski@hotmail.com>
2017-09-24 14:37:42 +0200

    Initial Commit

---
+++ SeagueTutorial/redViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeagueTutorial
+//
+//  Created by michael moldawski on 24/09/17.
+//  Copyright © 2017 michael moldawski. All rights reserved.
+//
+
+import UIKit
+
+class redViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
